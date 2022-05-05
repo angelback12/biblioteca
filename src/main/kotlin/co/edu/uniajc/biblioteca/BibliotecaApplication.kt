65c49e0785ee41158f9e7ec4b485d99d2462049a
angelback12 <angelback12@gmail.com>
2022-05-04 20:49:23 -0500

    Se Suber repositorio

---
+++ src/main/kotlin/co/edu/uniajc/biblioteca/BibliotecaApplication.kt
@@ -0,0 +1,11 @@
+package co.edu.uniajc.biblioteca
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BibliotecaApplication
+
+fun main(args: Array<String>) {
+	runApplication<BibliotecaApplication>(*args)
+}
