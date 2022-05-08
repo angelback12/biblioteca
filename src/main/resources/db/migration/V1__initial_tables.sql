824dc002f7e559fcb9b39ed19869483e630bb9bc
angelback12 <angelback12@gmail.com>
2022-05-08 15:23:38 -0500

    version de tabla

---
+++ src/main/resources/db/migration/V1__initial_tables.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "libro"(
+                        lb_id serial NOT NULL,
+                        lb_name character varying(50) NOT NULL,
+                        lb_desc character varying(100) NOT NULL,
+                        PRIMARY KEY (ld_id)
+                       );
+ALTER TABLE "libro" OWNER to postgres;
