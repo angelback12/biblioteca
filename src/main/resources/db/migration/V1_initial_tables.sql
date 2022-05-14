251884777756b034fa629372a74bf47c73ce83df
maicolnarvaez <maicolnarvaezj@gmail.com>
2022-05-14 18:30:56 -0500

    se agrega revista
    se agrega revista

--- src/main/resources/db/migration/V1_initial_tables.sql
+++ src/main/resources/db/migration/V1_initial_tables.sql
@@ -4,3 +4,10 @@ CREATE TABLE "libro"(
                         description character varying(100) NOT NULL,
                        );
 ALTER TABLE "libro" OWNER to postgres;
+
+CREATE TABLE "revista"(
+                        id serial NOT NULL,
+                        name character varying(50) NOT NULL,
+                        description character varying(100) NOT NULL,
+                       );
+ALTER TABLE "revista" OWNER to postgres;
